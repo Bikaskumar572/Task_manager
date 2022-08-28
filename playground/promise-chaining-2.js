52157c7fea602512d881f62f7172b2ec7fa0f529
Bikas Kumar <bikaskr572@gmail.com>
2022-08-28 18:40:55 +0530

    Initial commit

---
+++ playground/promise-chaining-2.js
@@ -0,0 +1,22 @@
+require('../src/db/mongoose')
+const Task = require('../src/models/task')
+
+// Task.findByIdAndDelete('63021a2171b6401f2c6daba3').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed: false})
+    return count
+}
+deleteTaskAndCount('63021a2171b6401f2c6daba3').then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+})
