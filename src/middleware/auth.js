9ecaaef2edec6ad7187a17c32515d85c5b78b50d
Bikas Kumar <bikaskr572@gmail.com>
2022-08-28 23:13:36 +0530

    adding features

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 
 const auth = async (req,  res, next) => {
     try {
-        const token = req.header('Authorization').replace('Beare', '')
+        const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, 'thisismydevcourse')
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
@@ -11,8 +11,8 @@ const auth = async (req,  res, next) => {
             throw new Error()
         }
 
+        req.token = token
         req.user = user
-        
         next()
 
     } catch (e) {
--- src/models/user.js
+++ src/models/user.js
@@ -53,7 +53,7 @@ userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, 'thisismydevcourse')
 
-    user.token = user.tokens.concat({ token })
+    user.tokens = user.tokens.concat({ token })
     await user.save()
 
     return token
