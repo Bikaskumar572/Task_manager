407c597c3285b36a2a021f267c86369372c5e811
Bikas Kumar <bikaskr572@gmail.com>
2022-08-29 19:44:51 +0530

    User Authentication added

--- src/index.js
+++ src/index.js
@@ -13,14 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port' + port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismydevcourse', { expiresIn: '7 days' })
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismydevcourse')
-    console.log(data)
-}
-myFunction()
